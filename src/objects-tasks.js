a55a54d3ba6b511747340d77c85c113494ac90b6
tearzday <biverdaniil@gmail.com>
2025-03-06 17:59:12 +0300

    feat: add mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,22 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const newObj = {};
+
+  objects.forEach((item) => {
+    const arrItem = Object.entries(item);
+    arrItem.forEach((element) => {
+      const [key, value] = element;
+      if (newObj[key]) {
+        newObj[key] += value;
+      } else {
+        newObj[key] = value;
+      }
+    });
+  });
+
+  return newObj;
 }
 
 /**
