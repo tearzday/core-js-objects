5f0662c764769d582c44101fbc1993c7b457bd9e
tearzday <biverdaniil@gmail.com>
2025-03-07 11:46:14 +0300

    feat: add makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -135,8 +135,23 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const lettersArr = Object.entries(lettersObject);
+  const result = [];
+
+  lettersArr.forEach((item) => {
+    const [symbol, index] = item;
+
+    if (index.length > 1) {
+      index.forEach((num) => {
+        result[num] = symbol;
+      });
+    } else {
+      result[index] = symbol;
+    }
+  });
+
+  return result.join('');
 }
 
 /**
